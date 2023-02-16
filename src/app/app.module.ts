706d16acf077a7bd4921f1648603fae73bd57318
saloniborhade <salonirborhade2019@gmail.com>
2023-02-16 13:15:41 +0530

    firstcomment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClassComponent } from './component/class/class.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassComponent
   ],
   imports: [
     BrowserModule,
